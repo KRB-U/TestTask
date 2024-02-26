69b401474535ff6f0fcb1cb4f441a9f1bab43cf9
unknown <rubikonspace@ukr.net>
2024-02-26 02:46:23 +0200

    add_filter_for_favorites

--- src/components/pages/Favorites/Favorites.jsx
+++ src/components/pages/Favorites/Favorites.jsx
@@ -1,18 +1,40 @@
+import { createSelector } from '@reduxjs/toolkit';
 import CarCard from 'components/CarCard/CarCard';
-import { ButtonLoadMore } from 'components/buttons/LoadMore/LoadMore';
-import { selectFavorite } from 'components/redux/selectors';
+import { CarFilter } from 'components/CarFilter/CarFilter';
+import { selectFavorite, selectFilters } from 'components/redux/selectors';
 import { useSelector } from 'react-redux';
+import { ContainerFavorite } from './Favorites.styled';
 
 function Favorites() {
   const favorites = useSelector(selectFavorite);
 
+  const {
+    make: makeFilter,
+    pricePerHour,
+    minMileage,
+    maxMileage,
+  } = useSelector(selectFilters);
+
+  const filteredCars = favorites.filter(car => {
+    const { make, rentalPrice, mileage } = car;
+
+    return (
+      (makeFilter === '' || make === makeFilter) &&
+      (pricePerHour === '' || rentalPrice <= pricePerHour) &&
+      (minMileage === '' || mileage >= parseInt(minMileage)) &&
+      (maxMileage === '' || mileage <= parseInt(maxMileage))
+    );
+  });
+
   return (
-    <div>
-      {favorites.map(car => (
-        <CarCard key={car.id} car={car} />
-      ))}
-      {favorites.length >= 12 && <ButtonLoadMore />}
-    </div>
+    <>
+      <CarFilter />
+      <ContainerFavorite>
+        {filteredCars.map(car => (
+          <CarCard key={car.id} car={car} />
+        ))}
+      </ContainerFavorite>
+    </>
   );
 }
 
